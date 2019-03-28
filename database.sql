b363f3c22483ef649f676f14a01c740d41bb6175
raghav garg <rohanji97@gmail.com>
2019-03-28 22:28:37 +0530

    working databse

--- database.sql
+++ database.sql
@@ -1,4 +1,7 @@
-PRAGMA foreign_keys = ON;
+DROP DATABASE IF EXISTS RAILWAY_MANAGEMENT;
+CREATE DATABASE RAILWAY_MANAGEMENT;
+USE RAILWAY_MANAGEMENT;
+
 
 CREATE TABLE STATIONS(Station_no int PRIMARY KEY,
 Station_name varchar(30),
@@ -6,22 +9,26 @@ City varchar(30),
 Station_master varchar(30),
 no_of_platforms int);
 
+CREATE TABLE COACH_DETAILS(Coach_Type int PRIMARY KEY,
+AC BOOLEAN);
+
+CREATE TABLE TRAIN_SCHEDULE(Train_no int PRIMARY KEY,
+Coach_A_quantity int,Coach_A_price int,
+Coach_B_quantity int,Coach_B_price int,
+Coach_C_quantity int,Coach_C_price int,
+Coach_D_quantity int,Coach_D_price int,
+Coach_E_quantity int,Coach_E_price int);
+
+
 CREATE TABLE TRAIN_INFO(Train_no int PRIMARY KEY,
 Train_name varchar(30),
 Source_station_no int,
 Destination_station_no int,
 Distance int,
-Track_type CHECK(Track_type IN('Broad_gauge','Narrow_gauge')),
+Track_type varchar(30) CHECK(Track_type IN('Broad_gauge','Narrow_gauge')),
 FOREIGN key(Source_station_no) REFERENCES STATIONS(Station_no) ON UPDATE CASCADE ON DELETE CASCADE,
-FOREIGN key(Destination_station_no) REFERENCES STATIONS(Station_no) ON UPDATE CASCADE ON DELETE CASCADE,
+FOREIGN key(Destination_station_no) REFERENCES STATIONS(Station_no) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-CREATE TABLE COACH_DETAILS(Coach_Type int PRIMARY KEY,
-)
 
-CREATE TABLE TRAIN_SCHEDULE(Train_no int PRIMARY KEY,
-Coach_A_quantity int,Coach_A_price int,
-Coach_B_quantity int,Coach_B_price int
-Coach_C_quantity int,Coach_C_price int
-Coach_D_quantity int,Coach_D_price int
-Coach_E_quantity int,Coach_E_price int);
+
