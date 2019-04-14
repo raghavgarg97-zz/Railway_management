297cc26fdc77688a389cac20e43dc3237ac6a753
VishwasLathi <lathivishwas@gmail.com>
2019-04-14 12:37:30 +0530

    minor bugs fixed

--- Login_work/pnr_status.php
+++ Login_work/pnr_status.php
@@ -1,144 +1,106 @@
 <?php
-include_once 'includes/db_connect.php';
+include_once './includes/db_connect.php';
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<title>Table V05</title>
-	<meta charset="UTF-8">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="img/train_list_images/icons/favicon.ico"/>
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="styles/train_list_vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="styles/train_list_fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="styles/train_list_vendor/animate/animate.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="styles/train_list_vendor/select2/select2.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="styles/train_list_vendor/perfect-scrollbar/perfect-scrollbar.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="styles/train_list_css/util.css">
-	<link rel="stylesheet" type="text/css" href="styles/train_list_css/main.css">
-<!--===============================================================================================-->
-</head>
-<body>
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+	<title>PNR Status HTML Template</title>
+
+	<!-- Google font -->
+	<link href="http://fonts.googleapis.com/css?family=Playfair+Display:900" rel="stylesheet" type="text/css" />
+	<link href="http://fonts.googleapis.com/css?family=Alice:400,700" rel="stylesheet" type="text/css" />
+
+
+	<!-- Bootstrap -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment-with-locales.js"></script>
+
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+	<script src="http://cdn.rawgit.com/Eonasdan/bootstrap-datetimepicker/a549aa8780dbda16f6cff545aeabc3d71073911e/src/js/bootstrap-datetimepicker.js"></script>
+
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet"/>
+
+	<link href="http://cdn.rawgit.com/Eonasdan/bootstrap-datetimepicker/a549aa8780dbda16f6cff545aeabc3d71073911e/build/css/bootstrap-datetimepicker.css" rel="stylesheet"/>
 	
-	<div class="limiter">
-		<div class="container-table100">
-			<div class="wrap-table100">
-				<div class="table100 ver1">
-				<table id="table1">
-								<thead>
-									<tr class="row100 head">
-										<th class="cell100 column2">Confirmation Status</th>
-										<th class="cell100 column3">Train name</th>
-										<th class="cell100 column4">Source Station</th>
-										<th class="cell100 column5">Destination station</th>
-										<th class="cell100 column6">Coach Type</th>
-										<th class="cell100 column7">Date</th>
 
-									</tr>
-								</thead>
-								<tbody>
-								<?php
-										
-										// $username=$_GET['username'];
-										// $source=$_POST[''];
-										// $destination=$_POST[''];
-										// $coach=$_POST[''];
-										// $date=$_POST[''];
-										$PNR = $_POST['PNR'];
-										$sq='SELECT * FROM BOOKING WHERE PNR_no='.$PNR.';';
-										$result=$mysqli->query($sq);
-										$row = $result->fetch_assoc();
-										$status=$row['Booking_Status'];
+	<!-- Custom stlylesheet -->
+	<link type="text/css" rel="stylesheet" href="styles/style.css" />
 
-										
-										if($status == "CNF"){
-											echo "<script type='text/javascript'>alert('Your booking is confirmed');</script>";
-										}
-										else{
-											$Train_no=$row['Train_no'];
-											$source_no=$row['Source_station'];
-											$dest_no=$row['Destination_station'];
-											$date=$row['Boarding_Date'];
-											$coach=$row['Coach_Type'];
-											
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
 
-											$sq = 'SELECT * FROM OVERALL_WAITING WHERE Train_no = '.$Train_no.' AND Dates = "'.$date.'" AND Coach_Type = "'.$coach.'" AND PNR_no='.$PNR.';';
-											echo "<script type='text/javascript'>alert('$sq');</script>";
-											$result = $mysqli->query($sq);
-											$row = $result->fetch_assoc();
-											$WL = $row['Wl_no'];
-											echo "<script type='text/javascript'>alert('Your waiting list number is $WL');</script>";
-										}
+</head>
 
-										// $dt = strtotime($date);
-										// $day = date("D", $dt);
-
-										// $sq = 'SELECT DISTINCT T.Train_no from 
-										// (SELECT Train_no,Sequence_number from RAILWAY_PATH,STATIONS where Station_name="'.$source.'" and RAILWAY_PATH.Station_no=STATIONS.Station_no  and Saturday_avail=true)T ,
-										// (SELECT Train_no,Sequence_number from RAILWAY_PATH,STATIONS where Station_name="'.$destination.'" and RAILWAY_PATH.Station_no=STATIONS.Station_no and Saturday_avail=true)S where T.Train_no=S.Train_no and T.Sequence_number <S.Sequence_number;';
-										// $result = $mysqli->query($sq);				
-										
-										// while ($row = $result->fetch_assoc()){
-										//     $c=$c+1;
-										//     echo '<tr class="row100 head">';
-										// 	echo '<td class="cell100 column2">'.$row["Train_no"].'</th>';
-										// 	echo '<td><input type = "radio" id="id1" name = "select" value = "1" required onclick="getAllData('.$c.')"></td>';
-										// 	echo '</tr>';
-										// }
-								?>
-								</tbody>
-							</table>
+<body>
+	<div id="booking" class="section">
+		<div class="section-center">
+			<div class="container">
+				<div class="row">
+					<div class="booking-form">
+						<div class="booking-bg">
+							<div class="form-header">
+								<h2>Welcome.</h2>
+								<p>Home Page</p>
+							</div>
+						</div>
 						
+						<?php
+							$PNR = $_POST['PNR'];
+							$username = $_GET['username'];
+							$sq='SELECT * FROM BOOKING WHERE PNR_no='.$PNR.';';
+							$result=$mysqli->query($sq);
+							$row = $result->fetch_assoc();
+							if(!empty($row)){
+								$status=$row['Booking_Status'];								
+								if($status == "CNF"){
+									echo "<script type='text/javascript'>alert('Your booking is confirmed');</script>";
+								}
+								else{
+									$Train_no=$row['Train_no'];
+									$source_no=$row['Source_station'];
+									$dest_no=$row['Destination_station'];
+									$date=$row['Boarding_Date'];
+									$coach=$row['Coach_Type'];
+									
+
+									$sq = 'SELECT * FROM OVERALL_WAITING WHERE Train_no = '.$Train_no.' AND Dates = "'.$date.'" AND Coach_Type = "'.$coach.'" AND PNR_no='.$PNR.';';
+									echo "<script type='text/javascript'>alert('$sq');</script>";
+									$result = $mysqli->query($sq);
+									$row = $result->fetch_assoc();
+									$WL = $row['Wl_no'];
+									echo "<script type='text/javascript'>alert('Your waiting list number is $WL');</script>";
+								}
+							}
+							else{
+								echo "<script type='text/javascript'>alert('The PNR number : $PNR does not exist');</script>";
+							}
+						?>
+						
+						<form action="home.php?username=<?php echo $username;?>" method="post">
+							<div class="form-btn">
+								<button class="submit-btn">Go Back to Home</button>
+							</div>
+						</form>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 
-<!-- <script type="text/javascript">
-   function getAllData(id_value){
-   var table = document.getElementById("table1");
-   Train_no = table.rows[id_value].cells[0].innerHTML;                
-   Train_name = table.rows[id_value].cells[1].innerHTML;
-    alert("You are being redirected......");
-    // ALso edit this link
-  window.location.href="../booking_data.php?Train_no="+Train_no+"&Train_name="+Train_name;
-   }
-</script> -->
-<!--===============================================================================================-->	
-	<script src="styles/train_list_vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
-	 <script src="styles/train_list_vendor/bootstrap/js/popper.js"></script>
-	<script src="styles/train_list_vendor/bootstrap/js/bootstrap.min.js"></script>
- <!-- =============================================================================================== -->
-	 <script src="styles/train_list_vendor/select2/select2.min.js"></script> 
-<!--===============================================================================================-->
-	<script src="styles/train_list_vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
-	<script>
-		$('.js-pscroll').each(function(){
-			var ps = new PerfectScrollbar(this);
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
 
-			$(window).on('resize', function(){
-				ps.update();
-			})
 
-			$(this).on('ps-x-reach-start', function(){
-				$(this).parent().find('.table100-firstcol').removeClass('shadow-table100-firstcol');
-			});
-
-			$(this).on('ps-scroll-x', function(){
-				$(this).parent().find('.table100-firstcol').addClass('shadow-table100-firstcol');
-			});
-
-		});
-	</script>
-<!--===============================================================================================-->
-	<script src="js/train_list_js/main.js"></script>
-
-</body>
 </html>
